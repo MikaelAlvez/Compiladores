b8bfb39dcb1141189291a81d0bc141e32f160451
Mikael Alves <JOSE.ALVES08240@alunos.ufersa.edu.br>
2025-11-22 20:49:40 -0300

    declaração packages

--- parser_tonto.py
+++ parser_tonto.py
@@ -21,3 +21,48 @@ def p_error(p):
     else:
         print("[ERRO] Fim inesperado do arquivo (EOF).")
 
+# ===============================================================
+#   REGRA INICIAL
+# ===============================================================
+
+def p_start(p):
+    """start : package"""
+    p[0] = p[1]
+
+
+# ===============================================================
+#   1. DECLARAÇÃO DE PACOTES
+# ===============================================================
+
+def p_package(p):
+    """package : PACKAGE IDENT LBRACE package_body RBRACE
+               | PACKAGE CLASS_NAME LBRACE package_body RBRACE"""
+    ontologia["packages"].append(p[2])
+    p[0] = ("package", p[2], p[4])
+
+
+
+def p_package_body(p):
+    """package_body : package_item_list
+                    | empty"""
+    p[0] = p[1]
+
+
+def p_package_item_list(p):
+    """package_item_list : package_item_list package_item
+                         | package_item"""
+    if len(p) == 2:
+        p[0] = [p[1]]
+    else:
+        p[1].append(p[2])
+        p[0] = p[1]
+
+
+def p_package_item(p):
+    """package_item : enum_decl
+                    | class_decl
+                    | datatype_decl
+                    | generalization
+                    | relation_external"""
+    #p[0] = p[1]
+
