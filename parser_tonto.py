3d3531702aff3d7a9948fdc080c8cd36ad5e3066
Mikael Alves <JOSE.ALVES08240@alunos.ufersa.edu.br>
2025-11-22 20:54:00 -0300

    declaração de class e types

--- parser_tonto.py
+++ parser_tonto.py
@@ -66,3 +66,55 @@ def p_package_item(p):
                     | relation_external"""
     #p[0] = p[1]
 
+
+# ===============================================================
+#   2. DECLARAÇÃO DE CLASSES
+# ===============================================================
+
+def p_class_decl(p):
+    """class_decl : CLASS_NAME COLON IDENT class_block"""
+    ontologia["classes"].append({"name": p[1], "stereotype": p[3]})
+    p[0] = ("class", p[1])
+
+
+def p_class_block(p):
+    """class_block : LBRACE class_body RBRACE
+                   | empty"""
+    pass
+
+
+def p_class_body(p):
+    """class_body : class_body class_element
+                  | class_element"""
+    pass
+
+
+def p_class_element(p):
+    """class_element : attribute
+                     | relation_internal"""
+    p[0] = p[1]
+
+
+def p_attribute(p):
+    """attribute : RELATION_NAME COLON IDENT
+                 | RELATION_NAME COLON CLASS_NAME
+                 | RELATION_NAME COLON NATIVE_TYPE
+                 | RELATION_NAME COLON NEW_DATATYPE"""
+    p[0] = ("attribute", p[1], p[3])
+
+
+# ===============================================================
+#   3. DECLARAÇÃO DE TIPOS DE DADOS
+# ===============================================================
+
+def p_datatype_decl(p):
+    """datatype_decl : NEW_DATATYPE LBRACE attr_list RBRACE"""
+    ontologia["datatypes"].append(p[1])
+    p[0] = ("datatype", p[1])
+
+
+def p_attr_list(p):
+    """attr_list : attribute
+                 | attr_list attribute"""
+    pass
+
